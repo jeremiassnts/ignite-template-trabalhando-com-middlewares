fdaa52a060a924b238197f75ec1076542618aded
Jeremias Santos <jeremiassnts3@gmail.com>
2021-08-29 21:37:26 -0300

    feat: checks create todos user availability

--- src/index.js
+++ src/index.js
@@ -24,7 +24,15 @@ function checksExistsUserAccount(request, response, next) {
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  if (user.pro || user.todos.length < 10) {
+    return next()
+  } else {
+    return response.status(403).json({
+      "error": 'Forbidden'
+    })
+  }
 }
 
 function checksTodoExists(request, response, next) {
